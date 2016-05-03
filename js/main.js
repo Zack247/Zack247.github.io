46724d37e2099fbd452c36a7a684a7fcef9fa853
Zack.Lynch <zlynch247@gmail.com>
2016-05-02 23:41:53 -0700

    MAY 2nd FINAL COMMIT

---
+++ js/main.js
@@ -0,0 +1,64 @@
+// $('.hamburger').on('click', function () {
+// 	if ( $("#nav_menu:first").is(":hidden")) {
+// 		$('#nav_menu').slideDown ('slow');
+// 	}
+// 	else {
+// 		$("#nav_menu").slideUp ('slow');
+// 	}
+// });
+
+
+// // var navDistance = $('.main-nav').offset().top;
+// // console.log( 'The nav distance is: ' + navDistance );
+
+
+// // sticky nav
+
+// // When the user scrolls down the page 
+// $(window).on('scroll', function () {
+//     console.log( $(this).scrollTop()  )
+//     // if the scrollTop value is greater than or equal to the navDistance
+//     if ( $(this).scrollTop() >= navDistance ) {
+//         // add the class scrolled
+//         $('.main-nav').addClass('scrolled');
+//     } else {
+//         // remove the scrolled class
+//         $('.main-nav').removeClass('scrolled');
+//     }
+// });
+
+
+// DownArrow Scrool
+
+// $(window).on('scroll', function () {
+//     console.log( $(this).scrollTop()  )
+//     // if the scrollTop value is greater than or equal to the navDistance
+//     if ( $(this).scrollTop() >= navDistance ) {
+//         // add the class scrolled
+//         $('.downArrow').addClass('scrolled');
+//     } else {
+//         // remove the scrolled class
+//         $('.downArrow').removeClass('scrolled');
+//     }
+// });
+// sticky nav end
+
+
+// $(document).ready(function(){
+// 2
+//   $(".toggle").click(function(){
+// 3
+//     $(".menu").slideToggle(700);
+// 4
+//   });
+// 5
+// });
+
+
+// $( document.body ).click(function () {
+//   if ( $( "div:first" ).is( ":hidden" ) ) {
+//     $( "div" ).slideDown( "slow" );
+//   } else {
+//     $( "div" ).hide();
+//   }
+// });
